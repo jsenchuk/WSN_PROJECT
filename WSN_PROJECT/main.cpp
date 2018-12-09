257277930210ec1ce03a59dea3a83877b91837bd
jsenchuk <jsenchuk53@gmail.com>
2018-12-09 16:25:30 -0600

    test

---
+++ WSN_PROJECT/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WSN_PROJECT
+//
+//  Created by Joe Senchuk on 12/9/18.
+//  Copyright © 2018 Joe Senchuk. All rights reserved.
+//
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+int main()
+{
+    cout << "Hello, World!\n";
+    return 0;
+}
